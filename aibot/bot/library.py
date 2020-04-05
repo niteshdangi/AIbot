e12e5becc5c1e28b453eb0157847d3ac5eec90e4
Nitesh Kumar <60287761+niteshdangi@users.noreply.github.com>
2020-04-05 10:04:57 +0000

    Create library.py

---
+++ aibot/bot/library.py
@@ -0,0 +1,4 @@
+library = [{"id":0,"name":"math","dept":"cse","code":"mth01","floor":2,"total":10,"available":5,"issueable":True},
+						{"id":1,"name":"rice","dept":"agriculture","code":"agr103","floor":1,"total":80,"available":6,"issueable":True},
+						{"id":2,"name":"os","dept":"cse","code":"cse316","floor":5,"total":20,"available":4,"issueable":True},
+						{"id":3,"name":"mth102","dept":"cse","code":"mth316","floor":5,"total":20,"available":4,"issueable":True}]
